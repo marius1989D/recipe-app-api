5a364eb984d00af70bd82d000b37c35948ad1056
Mariu <dumitrumarius@dumitrus-mbp.home>
2020-03-26 21:18:02 +0000

    added function for creating superuser

--- app/core/models.py
+++ app/core/models.py
@@ -15,6 +15,16 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custon user model that supports using email insted of username"""
     email = models.EmailField(max_length=255, unique=True)
